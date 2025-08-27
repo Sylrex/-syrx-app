f6fdbb99b1cc75a55aeaa0531ef573190dce8334
Sylrex <khamis.alrajab@gmail.com>
2025-08-27 20:11:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import Flask, send_from_directory
 app = Flask(__name__, static_folder='.')
 
 # الصفحة الرئيسية
-@app.route("/SYRXApp")
+@app.route("/")
 def home():
     return send_from_directory('.', 'index.html')
 
-# أي ملف آخر تحت نفس المسار
-@app.route("/SYRXApp/<path:filename>")
+# أي ملف آخر في نفس المجلد
+@app.route("/<path:filename>")
 def serve_files(filename):
     return send_from_directory('.', filename)
 
