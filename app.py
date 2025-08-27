91102c6040d3c9ed3439519b5a20197f3dce401d
Sylrex <khamis.alrajab@gmail.com>
2025-08-27 21:27:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,5 @@ def serve_static(filename):
     return send_file(filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Render يستخدم متغير PORT
+    app.run(debug=False, host='0.0.0.0', port=port)
